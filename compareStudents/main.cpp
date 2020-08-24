01fe2a5aed558b5c902f15e9530c8d9277726706
Felix Strobel <mail@felixstrobel.de>
2020-08-24 21:17:00 +0200

    make Students array great again

--- compareStudents/main.cpp
+++ compareStudents/main.cpp
@@ -12,12 +12,14 @@ int main()
     int numberOfStudents;
 
     std::cin >> numberOfStudents;
-    Student students[numberOfStudents];
+    Student* students = new Student[numberOfStudents];
 
     for (int i = 0; i < numberOfStudents; i++) {
         students[i].input();
         std::cout << students[i].calculateTotalScore() << std::endl;
     }
+
+    delete[] students;
     return 0;
 }
 
